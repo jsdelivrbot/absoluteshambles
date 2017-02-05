62949fc9c18ccde0d128f1ab1eb19ffdafabd1bd
tomsheedy <thomas2.sheedy@live.uwe.ac.uk>
2017-02-05 13:01:24 +0000

    Update absoluteshambles.js

--- absoluteshambles.js
+++ absoluteshambles.js
@@ -1,2 +1,18 @@
 
 alert('hello world');
+
+var url = 'https://absoluteradio.co.uk/absolute-radio/music/'
+
+var request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+    if (request.readyState === 4) {
+        if (request.status === 200) {
+            document.body.className = 'ok';
+            console.log(request.responseText);
+        } else {
+            document.body.className = 'error';
+        }
+    }
+};
+request.open("GET", url , true);
+request.send(null);
